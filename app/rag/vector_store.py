eaeb6b5f6167d6806af0bf108d74d8724057001e
Yamini-678 <yaminibisht678@gmail.com>
2026-07-06 19:43:28 +0530

    feature adding

--- app/rag/vector_store.py
+++ app/rag/vector_store.py
@@ -1,21 +1,25 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import FAISS
-import os
 
-def create_vector_store(text : str):
+embeddings = GoogleGenerativeAIEmbeddings(
+    model="models/embedding-001"
+)
+
+
+def create_vector_store(text: str):
+
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 500,
-        chunk_overlap = 100
+        chunk_size=500,
+        chunk_overlap=100
     )
 
     chunks = splitter.split_text(text)
 
-    embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    vector_db = FAISS.from_texts(
+        chunks,
+        embeddings
     )
-
-    vector_db = FAISS.from_texts(chunks , embeddings)
 
     vector_db.save_local("vector_db")
 
